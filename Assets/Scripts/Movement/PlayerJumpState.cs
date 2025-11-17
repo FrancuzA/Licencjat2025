7f3cd5c34dcf1ab642678fa86115ff03cf9f013f
Cyprian "Francuz" Arquier <carquier@edu.cdv.pl>
2025-11-17 20:38:20 +0100

    Movement and camera fix

--- Assets/Scripts/Movement/PlayerJumpState.cs
+++ Assets/Scripts/Movement/PlayerJumpState.cs
@@ -1,25 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerJumpState : State
 {
     private Rigidbody _rb;
+    private Transform mainBody;
     private float _timer;
+    private float _mouseSens = 0.5f;
+
     public PlayerJumpState(StateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
         _rb = _stateMachine.GetComponent<Rigidbody>();
         _rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+        mainBody = _rb.GetComponent<Transform>();
+        _timer = 0f;
     }
 
     public override void Update()
     {
+        _mouseSens = Dependencies.Instance.GetDependancy<CameraTilt>().mouseSensitivity;
+        float mouseX = Input.GetAxis("Mouse X");
+
+        // Use the SAME shared rotation from state machine
+        _stateMachine.CurrentRotationAngle += mouseX * _mouseSens * 300 * Time.fixedDeltaTime;
+
+        // Apply shared rotation
+        mainBody.rotation = Quaternion.Euler(0, _stateMachine.CurrentRotationAngle, 0);
+
         if (_timer < 0.2f)
         {
             _timer += Time.deltaTime;
-
             return;
         }
 
